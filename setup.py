7bd56e52b949bc19651b82a6e811361baac8e38c
Ottokar Tilk <ottokart@gmail.com>
2017-03-15 20:03:48 +0200

    readme fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='Beam search',
       version='0.1.0',
       description='Beam search for neural network sequence to sequence (encoder-decoder) models.',
-      long_description=open("README.txt").read(),
+      long_description=open("README.md").read(),
       author='Ottokar Tilk',
       author_email='ottokart@gmail.com',
       license="MIT",
