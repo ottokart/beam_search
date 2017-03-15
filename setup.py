c147cb89c129babd56a3ae88e8ed3069ac247b6c
Ottokar Tilk <ottokart@gmail.com>
2017-03-15 20:12:25 +0200

    added url to setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 setup(name='Beam search',
       version='0.1.0',
       description='Beam search for neural network sequence to sequence (encoder-decoder) models.',
-      long_description=open("README.md").read(),
+      long_description=open('README.md').read(),
       author='Ottokar Tilk',
       author_email='ottokart@gmail.com',
-      license="MIT",
+      url='https://github.com/ottokart/beam_search',
+      license='MIT',
       py_modules=['beam_search'],
       install_requires=[
-        "numpy",
+        'numpy',
       ],
      )
